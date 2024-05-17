f7bd5ae638004463121ae3f1088688c7b6d55c32
Dariusz Naploszek <dariusznaploszek@gmail.com>
2024-05-17 11:29:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
 resource "random_pet" "pet1" {
   length = 5
 }
+
+resource "random_pet" "pet2" {
+  length = 5
+}
+
+resource "random_pet" "pet3" {
+  length = 5
+}
+
+resource "random_pet" "pet4" {
+  length = 5
+}
