a820b86ca6124e5a388b9dc0d6d826b47b30ff09
Dariusz Naploszek <dariusznaploszek@gmail.com>
2024-08-13 11:54:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,23 @@ resource "random_pet" "pet2" {
   length = 5
 }
 
-resource "random_pet" "pet3" {
-  length = 5
-}
+# resource "random_pet" "pet3" {
+#  length = 5
+# }
 
 resource "random_pet" "spacelift" {
   length = 1
+}
+
+resource "random_pet" "spaceliftNew" {
+  length = 1
+}
+
+moved {
+  from = random_pet.spacelift
+  to = random_pet.spaceliftNew
+}
+
+removed { 
+  from = random_pet.pet3
 }
