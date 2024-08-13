4227a0869210b21607af83473a4acf43d36d26c7
Dariusz Naploszek <dariuszn@spacelift.io>
2024-08-13 11:59:29 +0200

    More changes

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
-resource "random_pet" "pet2" {
-  length = 5
+resource "random_pet" "new-pet" {
+ length = 5
 }
-
-# resource "random_pet" "pet3" {
-#  length = 5
-# }
 
 # resource "random_pet" "spacelift" {
 #  length = 1
