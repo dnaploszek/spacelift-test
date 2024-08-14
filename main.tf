07313001e7473c40e2ddbc63ced8e3cc127d239c
Dariusz Naploszek <dariuszn@spacelift.io>
2024-08-14 09:46:04 +0200

    Add all resources back

--- main.tf
+++ main.tf
@@ -2,22 +2,34 @@ resource "random_pet" "new-pet" {
  length = 5
 }
 
-# resource "random_pet" "spacelift" {
-#  length = 1
-# }
+resource "random_pet" "pet1" {
+  length = 1
+}
+
+resource "random_pet" "pet2" {
+  length = 1
+}
+
+resource "random_pet" "pet3" {
+  length = 1
+}
+
+resource "random_pet" "spacelift" {
+ length = 1
+}
 
 resource "random_pet" "spaceliftNew" {
   length = 1
 }
 
-moved {
-  from = random_pet.spacelift
-  to = random_pet.spaceliftNew
-}
-
-removed { 
-  from = random_pet.pet3
-}
+# moved {
+#   from = random_pet.spacelift
+#   to = random_pet.spaceliftNew
+# }
+#
+# removed {
+#   from = random_pet.pet3
+# }
 
 resource "random_pet" "lots_of_random_1" {
   length = 5
