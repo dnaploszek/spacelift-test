eb575688051d3ca411af82189fd4f4970919c7e4
Dariusz Naploszek <dariuszn@spacelift.io>
2024-12-11 08:52:50 +0100

    Add changes generator

---
+++ changes_generator.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Define the file name
+FILE="main.tf"
+
+# Loop 150 times
+for i in {1..150}
+do
+  # Append the resource block with the iteration number
+  echo "resource \"random_pet\" \"pet$i$i\" {" >> $FILE
+  echo "  length = 5" >> $FILE
+  echo "}" >> $FILE
+  echo "" >> $FILE
+
+  # Add the changes to Git
+  git add $FILE
+
+  # Commit the changes
+  git commit -m "Add random_pet resource pet$i$i"
+
+  git push
+
+  sleep 5
+
+done
+
+echo "Appended 150 random_pet resources to $FILE."
